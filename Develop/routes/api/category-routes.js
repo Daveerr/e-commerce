0e2ecbccf2080cf20e0ec23f1a08368e985c740f
David Rodriguez <Daviderrr10@gmail.com>
2023-06-29 16:02:34 -0400

    endpoint and ID route

--- Develop/routes/api/category-routes.js
+++ Develop/routes/api/category-routes.js
@@ -3,17 +3,36 @@ const { Category, Product } = require("../../models");
 
 // The `/api/categories` endpoint
 
-router.get("/", (req, res) => {
-  const categories = await Category.findAll({
-    include(product),
-  });
+router.get("/", async (_req, res) => {
+  try {
+    const categories = await Category.findAll({
+      include: Product,
+    });
+    res.status(200).json(categories);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: "Failed to retrieve categories" });
+  }
+
   // find all categories
   // be sure to include its associated Products
 });
 
-router.get("/:id", (req, res) => {
+router.get("/:id", async (req, res) => {
   // find one category by its `id` value
   // be sure to include its associated Products
+  try {
+    const categoryData = await Category.findByPk(req.params.id, {
+      include: Product,
+    });
+    if (!categoryData) {
+      res.status(404).json({ message: "No category for this ID" });
+      return;
+    }
+    res.status(200).json(categoryData);
+  } catch (error) {
+    res.status(500).json(error);
+  }
 });
 
 router.post("/", (req, res) => {
