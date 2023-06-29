9fce493dfefebb4c55f8f817663d3b22abb11713
David Rodriguez <Daviderrr10@gmail.com>
2023-06-29 16:27:38 -0400

    routes code

--- Develop/routes/api/category-routes.js
+++ Develop/routes/api/category-routes.js
@@ -35,22 +35,54 @@ router.get("/:id", async (req, res) => {
   }
 });
 
-router.post("/", (req, res) => {
+router.post("/", async (req, res) => {
   // create a new category
   try {
-  } catch (error) {}
+    const categoryData = await Category.create(req.body);
+    res.status(200).json(categoryData);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: "failed to create new category" });
+  }
 });
 
-router.put("/:id", (req, res) => {
+router.put("/:id", async (req, res) => {
   // update a category by its `id` value
   try {
-  } catch (error) {}
+    const categoryData = await Category.update(req.body, {
+      where: {
+        id: req.params.id,
+      },
+    });
+
+    if (!categoryData[0]) {
+      res.status(404).json({ meesage: "No category with this ID" });
+      return;
+    }
+
+    res.status(200).json(categoryData);
+  } catch (error) {
+    res.status(500).json(error);
+  }
 });
 
-router.delete("/:id", (req, res) => {
+router.delete("/:id",  (req, res) => {
   // delete a category by its `id` value
   try {
-  } catch (error) {}
+    const categoryData = await Category.destroy({
+      where: {
+        id: req.params.id,
+      },
+    });
+
+    if (!categoryData) {
+      res.status(404).json({message: 'No category with that ID'});
+      return;
+    };
+  } catch (error) {
+    res.status(500).json(error);
+
+  }
 });
 
 module.exports = router;
